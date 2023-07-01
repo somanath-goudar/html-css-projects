d8a85195c3f48f364edb519b9ee54ccd5bcf2a29
Utsav Saxena <utsavmra2004@gmail.com>
2023-07-01 09:27:47 +0530

    made changes to look similar to the latest youtube version

--- youtube-clone/index.js
+++ youtube-clone/index.js
@@ -6,3 +6,10 @@ const sidebar = document.querySelector('.sidebar');
 menu.addEventListener('click', function () {
   sidebar.classList.toggle('show-sidebar');
 });
+
+const YtReload = document.querySelector('#yt-btn');
+console.log(YtReload);
+
+YtReload.addEventListener('click', function () {
+  location.reload(false);
+});
